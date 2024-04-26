47b144ebc52fa99a9e9b6daeefb59bd8bbab9bac
李振主 <LEON0317@HFCFinance.com.tw>
2024-04-26 17:32:38 +0800

    更新＿技能樹

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 //使用頻率%
 var skillPCNT1,skillPCNT2,skillPCNT3,skillPCNT4,skillPCNT5,skillPCNT6,skillPCNT7,skillPCNT8;
-skillPCNT1='80',skillPCNT2='75', skillPCNT3='85',skillPCNT4='80',skillPCNT5='90',skillPCNT6='100',skillPCNT7='80',skillPCNT8='80';
+skillPCNT1='80',skillPCNT2='75', skillPCNT3='85',skillPCNT4='80',skillPCNT5='90',skillPCNT6='100',skillPCNT7='85',skillPCNT8='80';
 var skillNM1,skillNM2,skillNM3,skillNM4,skillNM5,skillNM6,skillNM7,skillNM8;
-skillNM1='HTML',skillNM2='CSS',skillNM3='TypeScript',skillNM4='JavaScript',skillNM5='jQuery',skillNM6='SQL Server',skillNM7='C# Language',skillNM8='VB Language'
+skillNM1='HTML',skillNM2='CSS',skillNM3='TypeScript',skillNM4='JavaScript',skillNM5='jQuery',skillNM6='Transact-SQL',skillNM7='C# Language',skillNM8='VB Language'
 $('.skillPCNT1').find('strong').text(skillNM1);
 $('.skillPCNT1').append(skillPCNT1+'%');
 $('.dPCNT1').attr('data-percent',skillPCNT1);
@@ -37,5 +37,12 @@ $('.skillNM5').text(skillNM5);
 $('.skillNM6').text(skillNM6);
 $('.skillNM7').text(skillNM7);
 $('.skillNM8').text(skillNM8);
-
-$('.skillNM1').prev().addClass('AAA');
+//長條圖
+$('.skillNM1').parent().parent().find('.progress').find('.active').width(skillPCNT1+'%').attr('aria-valuenow',skillPCNT1+'%');
+$('.skillNM2').parent().parent().find('.progress').find('.active').width(skillPCNT2+'%').attr('aria-valuenow',skillPCNT2+'%');
+$('.skillNM3').parent().parent().find('.progress').find('.active').width(skillPCNT3+'%').attr('aria-valuenow',skillPCNT3+'%');
+$('.skillNM4').parent().parent().find('.progress').find('.active').width(skillPCNT4+'%').attr('aria-valuenow',skillPCNT4+'%');
+$('.skillNM5').parent().parent().find('.progress').find('.active').width(skillPCNT5+'%').attr('aria-valuenow',skillPCNT5+'%');
+$('.skillNM6').parent().parent().find('.progress').find('.active').width(skillPCNT6+'%').attr('aria-valuenow',skillPCNT6+'%');
+$('.skillNM7').parent().parent().find('.progress').find('.active').width(skillPCNT7+'%').attr('aria-valuenow',skillPCNT7+'%');
+$('.skillNM8').parent().parent().find('.progress').find('.active').width(skillPCNT8+'%').attr('aria-valuenow',skillPCNT8+'%');
