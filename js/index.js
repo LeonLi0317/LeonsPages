e422f6c83ad29a4d838c03aa14cc39fe0cae900c
李振主 <LEON0317@HFCFinance.com.tw>
2024-09-12 17:42:58 +0800

    uuu

--- js/index.js
+++ js/index.js
@@ -36,14 +36,6 @@ function getSkill(){
 };
 
 function SetCwinHeight() {
-    var iframeid = document.getElementById("msit-youtube"); //iframe id  
-    if (document.getElementById) {
-        if (iframeid && !window.opera) {
-            if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
-                iframeid.height = iframeid.contentDocument.body.offsetHeight;
-            } else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
-                iframeid.height = iframeid.Document.body.scrollHeight;
-            }
-        }
-    }
+    var ifrm = document.getElementById("player-youtube");
+    ifrm.height = document.body.scrollHeight;
 }
